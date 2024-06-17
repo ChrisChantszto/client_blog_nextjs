f9476133b73cc77307aecb345f02bc1eefe42c6c
Mr. CHAN Tsz To <ttchan67@ad.cityu.edu.hk>
2024-06-17 16:12:33 +0800

    ban

--- components/Postlist/Postlist.tsx
+++ components/Postlist/Postlist.tsx
@@ -1,91 +1,3 @@
-// import { Text, Title, Divider, Image, Stack, Button, Grid, Badge, Center } from "@mantine/core";
-// import { IconPlus } from "@tabler/icons-react";
-
-// export default function Postlist() {
-//     return(
-//         <>
-//             <Title fw={800} order={1} c="orange">SELECTED POST</Title>
-//             <Title fw={800} order={2}>精選文章</Title>
-//             <br />
-//             <Divider size="sm" />
-//             <br />
-//             <Stack
-//                 h={1000}
-//                 bg="var(--mantine-color-body)"
-//                 align="stretch"
-//                 justify="flex-start"
-//                 gap="lg"
-//             >
-//                 <Grid>
-//                     <Grid.Col span={4}>
-//                         <Image
-//                             radius="md"
-//                             h={200}
-//                             w="auto"
-//                             fit="contain"
-//                             src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
-//                         />
-//                     </Grid.Col>
-//                     <Grid.Col span={8}>
-//                         <Badge color="#FF6031">最新</Badge>
-//                         <Title order={5}>May 09, 2024  / By Venus Law</Title>
-//                         <Title order={2}>九龍海逸君綽酒店$88起歎海雲天點心！飽覽210度維港景色＋10大至尊點心連＋任食番石榴甘露糖水</Title>
-//                     </Grid.Col>
-//                 </Grid>
-//                 <Grid>
-//                     <Grid.Col span={4}>
-//                         <Image
-//                             radius="md"
-//                             h={200}
-//                             w="auto"
-//                             fit="contain"
-//                             src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
-//                         />
-//                     </Grid.Col>
-//                     <Grid.Col span={8}>
-//                         <Badge color="#FF6031">最新</Badge>
-//                         <Title order={5}>May 09, 2024  / By Venus Law</Title>
-//                         <Title order={2}>九龍海逸君綽酒店$88起歎海雲天點心！飽覽210度維港景色＋10大至尊點心連＋任食番石榴甘露糖水</Title>
-//                     </Grid.Col>
-//                 </Grid>
-//                 <Grid>
-//                     <Grid.Col span={4}>
-//                         <Image
-//                             radius="md"
-//                             h={200}
-//                             w="auto"
-//                             fit="contain"
-//                             src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
-//                         />
-//                     </Grid.Col>
-//                     <Grid.Col span={8}>
-//                         <Badge color="#FF6031">最新</Badge>
-//                         <Title order={5}>May 09, 2024  / By Venus Law</Title>
-//                         <Title order={2}>九龍海逸君綽酒店$88起歎海雲天點心！飽覽210度維港景色＋10大至尊點心連＋任食番石榴甘露糖水</Title>
-//                     </Grid.Col>
-//                 </Grid>
-//                 <Grid>
-//                     <Grid.Col span={4}>
-//                         <Image
-//                             radius="md"
-//                             h={200}
-//                             w="auto"
-//                             fit="contain"
-//                             src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
-//                         />
-//                     </Grid.Col>
-//                     <Grid.Col span={8}>
-//                         <Badge color="#FF6031">最新</Badge>
-//                         <Title order={5}>May 09, 2024  / By Venus Law</Title>
-//                         <Title order={2}>九龍海逸君綽酒店$88起歎海雲天點心！飽覽210度維港景色＋10大至尊點心連＋任食番石榴甘露糖水</Title>
-//                     </Grid.Col>
-//                 </Grid>
-//             </Stack>
-//             <Center><Button variant="filled" color="#FF6031" radius="xl"><Text size="xl">更多文章</Text><IconPlus /></Button></Center>
-//         </>
-//     )
-// }
-
 import { useEffect, useState } from "react";
 import { Text, Title, Divider, Image, Stack, Button, Grid, Badge, Center } from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
@@ -127,43 +39,54 @@ export default function Postlist() {
 
   return (
     <>
-      <Title fw={800} order={1} c="orange">SELECTED POST</Title>
-      <Title fw={800} order={2}>精選文章</Title>
-      <br />
-      <Divider size="sm" />
-      <br />
-      <Stack
-        h={1000}
-        bg="var(--mantine-color-body)"
-        align="stretch"
-        justify="flex-start"
-        gap="lg"
-      >
-        {posts.map((post) => (
-          <Grid key={post.title}>
-            <Grid.Col span={4}>
-              <Image
-                radius="md"
-                h={200}
-                w="auto"
-                fit="contain"
-                src={post.featured_image}
-              />
-            </Grid.Col>
-            <Grid.Col span={8}>
-              <Badge color="#FF6031">最新</Badge>
-              <Title order={5}>{new Date(post.date).toLocaleDateString()} / By {post.author}</Title>
-              <Title order={2}>{post.title}</Title>
-            </Grid.Col>
-          </Grid>
-        ))}
-      </Stack>
-      <Center>
-        <Button variant="filled" color="#FF6031" radius="xl">
-          <Text size="xl">更多文章</Text>
-          <IconPlus />
-        </Button>
-      </Center>
+        <Title fw={800} order={1} c="orange">SELECTED POST</Title>
+        <Title fw={800} order={2}>精選文章</Title>
+        <br />
+        <Divider size="sm" />
+
+        <br />
+        <Grid>
+      <Grid.Col span={10}>
+        <Stack
+          h={1000}
+          bg="var(--mantine-color-body)"
+          align="stretch"
+          justify="flex-start"
+          gap="lg"
+        >
+          {posts.map((post) => (
+            <Grid key={post.title}>
+              <Grid.Col span={4}>
+                <Image
+                  radius="md"
+                  h={200}
+                  w="auto"
+                  fit="contain"
+                  src={post.featured_image}
+                />
+              </Grid.Col>
+              <Grid.Col span={8}>
+                <Badge color="#FF6031">最新</Badge>
+                <Title order={5}>{new Date(post.date).toLocaleDateString()} / By {post.author}</Title>
+                <Title order={2}>{post.title}</Title>
+              </Grid.Col>
+            </Grid>
+          ))}
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={2}>
+        <div style={{ position: 'sticky', top: '10px', marginTop: '1rem' }}>
+          <Image
+            radius="md"
+            src="https://via.placeholder.com/200x400.png?text=Banner"
+            alt="Banner"
+            fit="contain"
+            style={{ width: '70%', height: 'auto' }}
+          />
+        </div>
+      </Grid.Col>
+    </Grid>
+    
     </>
   );
 }
