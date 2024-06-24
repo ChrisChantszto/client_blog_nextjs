80836186a4c7806a9c7200d8352500696b351c27
ChrisChantszto <chantszto.chris@gmail.com>
2024-06-25 07:43:28 +0800

    fds

--- theme.ts
+++ theme.ts
@@ -2,10 +2,4 @@ import { createTheme, virtualColor } from '@mantine/core';
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
-  colors: {
-    primary: virtualColor({
-      name: 'primary',
-      light: '#F5F5F5',
-    }),
-  },
 });
